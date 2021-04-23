436aba36ff88215063b9d21bf2698ee958a66f28
Benny Wang <bennywang75@hotmail.com>
2021-04-22 19:51:52 -0700

    Basic link generate with no style

--- app.js
+++ app.js
@@ -1,52 +1,19 @@
-let casterLevelInput = document.getElementById("casterLevel")
-let initResistInput = document.getElementById("initResist")
-let fianlResistInput = document.getElementById("finalResist")
-let ouptutSpan = document.getElementById("output")
+const BASE_URL = "https://classic.wowhead.com/quest="
 
-function outputError(msg) {
-    console.error(`Error: ${msg}`)
-    ouptutSpan.textContent = `Error: ${msg}`
-    ouptutSpan.style.color = "red";
-}
-
-function outputMsg(msg) {
-    console.log(msg)
-    ouptutSpan.textContent = msg
-    ouptutSpan.style.color = "white"
-}
-
-function averageDamageAbsorb(targetResistance, casterLevel) {
-    return 1 / (1 - ((targetResistance / (casterLevel * 5)) * 0.75)) - 1
-}
-
-
-function calcButtonClicked() {
-    // get values
-    const casterLevelValue = parseInt(casterLevelInput.value, 10);
-    const initResistValue = parseInt(initResistInput.value, 10);
-    const finalResistValue = parseInt(fianlResistInput.value, 10);
-
-    // error handling
-    if (isNaN(casterLevelValue) || isNaN(initResistValue) || isNaN(finalResistValue)) {
-        outputError("Invalid resist values");
-        return
-    }
-
-    if (casterLevelValue < 1) {
-        outputError("Mobs cannot be lower than level 1")
-        return
-    }
-
-    if (initResistValue < 1 || initResistValue > 315 ||
-        finalResistValue < 1 || finalResistValue > 315) {
-        outputError("Invalid amount of resist")
-    }
-
-    // calculate
-    deltaResist = averageDamageAbsorb(finalResistValue, casterLevelValue) / averageDamageAbsorb(initResistValue, casterLevelValue)
-    console.log(deltaResist)
-    deltaResistString = `${Number(100 * deltaResist).toFixed()}%`
-
-    // output
-    outputMsg(`Change in damage mitigation is ${deltaResistString}`)
+function generateLinks() {
+    const outputDiv = document.getElementById("output")
+    const rawString = document.getElementById("questIdInputField").value
+    const tokens = rawString.trim().split("\n")
+    
+    outputDiv.innerHTML = "";
+    tokens.forEach((questId) => {
+        const questUrl = BASE_URL + questId
+        let a = document.createElement("a")
+        linkText = document.createTextNode(questUrl)
+        a.appendChild(linkText)
+        a.title = questUrl
+        a.href = questUrl
+        outputDiv.appendChild(a)
+        outputDiv.appendChild(document.createElement("br"))
+    })
 }
