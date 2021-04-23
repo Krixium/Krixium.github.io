0d3fa565b4ab135216364a6df8f86b72afd81ea1
Benny Wang <bennywang75@hotmail.com>
2021-04-22 20:22:41 -0700

    Made it look kind nicer

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 const BASE_URL = "https://classic.wowhead.com/quest="
 
+function stringToA(linkString) {
+    let a = document.createElement("a")
+    a.appendChild(document.createTextNode(linkString))
+    a.title = linkString
+    a.href = linkString
+    return a;
+}
+
 function generateLinks() {
     const outputDiv = document.getElementById("output")
     const rawString = document.getElementById("questIdInputField").value
     const tokens = rawString.trim().split("\n")
-    
+
     outputDiv.innerHTML = "";
+    outputDiv.appendChild(document.createElement("br"))
+    outputDiv.appendChild(document.createElement("br"))
+
     tokens.forEach((questId) => {
-        const questUrl = BASE_URL + questId
-        let a = document.createElement("a")
-        linkText = document.createTextNode(questUrl)
-        a.appendChild(linkText)
-        a.title = questUrl
-        a.href = questUrl
-        outputDiv.appendChild(a)
-        outputDiv.appendChild(document.createElement("br"))
+        if (questId != "") {
+            const questUrl = BASE_URL + questId
+            outputDiv.appendChild(stringToA(questUrl))
+            outputDiv.appendChild(document.createElement("br"))
+        }
     })
 }
